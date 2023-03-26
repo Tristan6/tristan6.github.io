452e426294b6c8a6189bfb0fc59c4c505b46a52e
Tristan Macelli <tristanmacelli@gmail.com>
2023-03-26 15:07:28 -0500

    [CHANGE] package updates && deploy script fiz

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     extend: {
       colors: {
         highlight: '#bf5700',
+        lightSand: '#E6BAA3',
+        darkSand: '#BF9E87',
+      },
+      backgroundImage: {
+        'picacho': "url('@/assets/img/picacho-sidestream.png')",
+        'picacho-extended': "url('@/assets/img/picacho-sidestream-extended.png')",
       },
     }
   },
