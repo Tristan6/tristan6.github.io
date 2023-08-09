59fa251c376deb1c5d01742d0b9df768ccbe13bb
Tristan Macelli <tristanmacelli@gmail.com>
2023-08-09 15:40:15 -0500

    [UPDATE] configuration to prevent node_modules lint && type check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
   root: true,
 
-  env: {
-    esnext: true
-  },
-
   parser: "vue-eslint-parser",
 
   parserOptions: {
-    ecmaVersion: "2020",
+    ecmaVersion: "latest",
     parser: "@typescript-eslint/parser",
     exclude: ["node_modules"],
   },
 
-  plugins: ["@typescript-eslint"],
+  plugins: ["prettier", "@typescript-eslint"],
 
   extends: [
     "plugin:vue/vue3-essential",
+    "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/typescript",
-    "eslint:recommended"
   ],
 };
